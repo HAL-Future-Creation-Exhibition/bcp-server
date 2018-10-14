284aed8eeccb902feef8dd14938cbd1aafb92463
ohtukasyota <syota.jaska@gmail.com>
2018-10-14 20:32:55 +0900

    .

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/syota-ohtuka/bcp-server/controller"
+	"github.com/HAL-Future-Creation-Exhibition/bcp-server/controller"
 )
 
 func main() {
