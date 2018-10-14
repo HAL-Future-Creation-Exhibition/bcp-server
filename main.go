1631a04d037426c037b6e50220344acc774f01fd
ohtukasyota <syota.jaska@gmail.com>
2018-10-14 20:23:29 +0900

    .

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/syota-ohtuka/bcp-server/controller"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/alive", controller.Bcp.Alive)
+	r.GET("/connect", controller.Bcp.Connect)
+	r.GET("/disconnect", controller.Bcp.Disconnect)
+	r.Run() // listen and serve on 0.0.0.0:8080
+}
